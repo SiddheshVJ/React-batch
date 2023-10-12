bec452fcb295be2b61531ebe450593d047286530
Siddhesh <siddheshvjadhav20@gmail.com>
2023-10-12 18:01:29 +0530

    Added class components with style

---
+++ first-app/src/components/MessageCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class MessageCard extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className='card'>
+                    <h1>Good Morning</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sed consequatur doloribus magnam exercitationem illum enim. Nulla, id. Harum asperiores consectetur minus optio nemo eaque fugiat reiciendis repellat aliquid impedit!</p>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MessageCard;
---
+++ first-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <nav className="navbar">
+                    <a href="#">React Course</a>
+                </nav>
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default Navbar
