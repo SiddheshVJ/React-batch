e8ec4858b5cdf60c831cc295a54af60e5a621400
Siddhesh <siddheshvjadhav20@gmail.com>
2023-10-13 19:22:05 +0530

    Added CountryCards,CountryCard componentss

--- react-bootstrap/src/components/CountryCard.js
+++ react-bootstrap/src/components/CountryCard.js
@@ -4,7 +4,15 @@ class CountryCard extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <h1>CountryCard</h1>
+                <div className="col-md-2 col-md-4 col-lg-4">
+                    <div className="card m-3 row  ">
+                        <img src={this.props.cardImg} alt='' className=" card-img-top" />
+                        <div className="card-body">
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime pariatur ullam officia exercitationem id laudantium rerum, sequi fugiat quam ad commodi suscipit labore quidem? Esse consectetur cum quis illo a.</p>
+                            <button className='btn btn-amber'>Read More</button>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
--- react-bootstrap/src/components/CountryCards.js
+++ react-bootstrap/src/components/CountryCards.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import CountryCard from './CountryCard';
-
+import img1 from '../assets/images/img1.png'
+import img2 from '../assets/images/img2.png'
+import img3 from '../assets/images/img3.png'
+import img4 from '../assets/images/img4.png'
+import img5 from '../assets/images/img5.png'
 
 class CountryCards extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <CountryCard />
+                <div className='text-center d-flex flex-sm-column flex-lg-row flex-lg-wrap '>
+                    <CountryCard cardImg={img1} />
+                    <CountryCard cardImg={img2} />
+                    <CountryCard cardImg={img3} />
+                    <CountryCard cardImg={img4} />
+                    <CountryCard cardImg={img5} />
+                    <CountryCard cardImg={img5} />
+                </div>
             </React.Fragment>
         )
     }
