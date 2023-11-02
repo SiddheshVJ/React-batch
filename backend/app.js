54b980dc67252749f2434a320ac2211f9904efbd
Siddhesh <siddheshvjadhav20@gmail.com>
2023-11-02 21:06:42 +0530

    json webtoken

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,42 @@ import bodyParser from "body-parser";
 import bcrypt from "bcryptjs";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
 
 dotenv.config()
 let app = express()
 let port = process.env.PORT || 3113
+let secretKey = process.env.SECRETE
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// heartbit
+// let login data
 
+let user = {
+    id: 'Sid123',
+    name: 'Siddhesh',
+    email: 'sid@gmail.com',
+    password: 'Sidd3102'
+}
+
+let playLoad = {
+    user: {
+        id: user.id,
+        name: user.name
+    }
+}
+
+// create token
+let token = jwt.sign(playLoad, secretKey, { expiresIn: 300000000 })
+console.log(token)
+
+// decode token at server using this
+let decode = jwt.verify(token, secretKey)
+console.log(decode)
+
+// heartbit
 app.get('/heartbit', (req, res) => {
     res.status(200).send("<h1>Health is OK.</h1>")
 })
