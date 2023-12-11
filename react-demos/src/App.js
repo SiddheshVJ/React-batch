0bbd5394642bdf71d1515e6c8c59675ad1cc3d69
Siddhesh <siddheshvjadhav20@gmail.com>
2023-12-11 10:15:02 +0530

    useReducer()

--- react-demos/src/App.js
+++ react-demos/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import { ComponentB } from "./component/CoponenetB";
+import React from "react";
+import './App.css'
 
 let App = () => {
 
   return (
     <>
-      <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
-        <div className="container">
-          <a href="/" className="navbar-brand">Component Interaction</a>
-        </div>
-      </nav>
-      <p className="h4">App Component</p>
-      {/* <ComponentB /> */}
+    
     </>
   )
 }
--- react-demos/src/index.js
+++ react-demos/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import 'mdbootstrap/css/bootstrap.min.css'
 import 'mdbootstrap/css/mdb.css'
